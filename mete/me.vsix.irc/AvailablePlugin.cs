d2417c6c856416236abfa4e36bc49148724a13d6
pete.grace <pete.grace@0a2da81a-310e-11de-8a2e-13aa1706fec1>
2009-04-24 21:00:31 +0000

    initial commit of irc base source code into google svn.

---
+++ mete/me.vsix.irc/AvailablePlugin.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using me.vsix.irc;
+using System;
+
+[Serializable]
+public struct AvailablePlugin
+{
+    public string AssemblyPath;
+    public string ClassName;
+    public IRCPlugin instance;
+    public AppDomain domain;
+    public bool naughty;
+    public ModuleImplements implements;
+
+}
---
+++ mete/me.vsix.irc/IRCPlugin.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace me.vsix.irc
+{
+    //public delegate void myDelegate(int actionType, ref object obj);
+    [Serializable]
+    public struct pReply
+    {
+        public ReplyTypes type;
+        public string[] args;
+    };
+
+    public enum ReplyTypes
+    {
+        Nothing = 0,
+        PrivMsg = 1,
+        Mode = 2
+    }
+public enum ModuleImplements
+{
+    Nothing =0,
+    Chat = 1,
+    Mode = 2,
+    Join = 4,
+    Kick = 8
+}
+    public interface IRCPlugin
+    {
+
+        //event myDelegate ActionRequired;
+        string pGetTriggerRegexp(int whichType);
+        bool pEntryPoint(string sender, string inputStr);
+        pReply pGetPendingSends();
+        ModuleImplements pGetModuleJobs();
+        bool pModeCheck(string sender, string inputStr);
+        bool pKickCheck(string sender, string inputStr);
+        bool pJoinCheck(string sender, string hostmask, string dest, string data);
+
+    }
+}
